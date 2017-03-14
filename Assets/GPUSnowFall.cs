20f35cdb962b4f733daa4e910b721f64fcf93d94
umeda <umeda@team-lab.com>
2017-03-14 23:13:25 +0900

    First Commit.

---
+++ Assets/GPUSnowFall.cs
@@ -0,0 +1,218 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class GPUSnowFall : MonoBehaviour
+{
+    #region Field
+
+    /// <summary>
+    /// 雪を生成して描画するシェーダ。
+    /// </summary>
+    public Shader gpuSnowFallShader;
+
+    /// <summary>
+    /// 雪を動かすコンピュートシェーダ。
+    /// </summary>
+    public ComputeShader gpuSnowFallComputeShader;
+
+    /// <summary>
+    /// 雪のパーティクルの数。
+    /// </summary>
+    [Range(0, 1000000)]
+    public int particleCount = 1000;
+
+    /// <summary>
+    /// 雪を降らせる最小の位置。
+    /// </summary>
+    public Vector3 minRange;
+
+    /// <summary>
+    /// 雪を降らせる最大の位置。
+    /// </summary>
+    public Vector3 maxRange;
+
+    /// <summary>
+    /// 雪の最小のサイズ。
+    /// </summary>
+    public float minScale;
+
+    /// <summary>
+    /// 雪の最大のサイズ。
+    /// </summary>
+    public float maxScale;
+
+    /// <summary>
+    /// 雪の流れる方向(風向き)。
+    /// </summary>
+    public Vector3 wind = new Vector3(0, -0.1f, 0);
+
+    /// <summary>
+    /// 雪の描画を実行するマテリアル。
+    /// </summary>
+    private Material material;
+
+    /// <summary>
+    /// 雪の座標情報を持つ GPU 上のバッファ。
+    /// </summary>
+    private ComputeBuffer vertexBuffer;
+
+    /// <summary>
+    /// 雪のサイズ情報を持つ GPU 上のバッファ。
+    /// </summary>
+    private ComputeBuffer scaleBuffer;
+
+    /// <summary>
+    /// 雪を動かすカーネル(関数)の名前。
+    /// </summary>
+    private static readonly string kernelNameSnowFall = "SnowFall";
+
+    /// <summary>
+    /// 雪を動かすカーネル(関数)のインデックス。
+    /// </summary>
+    private int kernelIndexSnowFall;
+
+    /// <summary>
+    /// 雪を動かすカーネル(関数)の実行スレッド数。
+    /// </summary>
+    private Vector3 kernelThreadGroupSizeSnowFall;
+
+    #endregion Field
+
+    #region Method
+
+    /// <summary>
+    /// 開始時に呼び出されます。
+    /// </summary>
+    protected virtual void Start()
+    {
+        // (1) ランダムな位置に雪の座標を生成して VertexBuffer に登録します。
+
+        Vector3[] vertices = GenerateRandomPositionVertexes
+            (this.particleCount, this.minRange, this.maxRange);
+
+        this.vertexBuffer = new ComputeBuffer(this.particleCount, Marshal.SizeOf(typeof(Vector3)));
+        this.vertexBuffer.SetData(vertices);
+
+        this.scaleBuffer = new ComputeBuffer(this.particleCount, Marshal.SizeOf(typeof(float)));
+        this.scaleBuffer.SetData(GenerateRandomValues(this.particleCount, this.minScale, this.maxScale));
+
+        // VertexBuffer の登録。
+
+        this.material = new Material(this.gpuSnowFallShader);
+        this.material.SetBuffer("_VertexBuffer",  this.vertexBuffer);
+        this.material.SetBuffer("_ScaleBuffer",   this.scaleBuffer);
+
+        // ComputeShader の初期化と VertexBuffer の登録。
+
+        this.kernelIndexSnowFall = this.gpuSnowFallComputeShader.FindKernel(GPUSnowFall.kernelNameSnowFall);
+
+        uint sizeX, sizeY, sizeZ;
+        this.gpuSnowFallComputeShader.GetKernelThreadGroupSizes(this.kernelIndexSnowFall, out sizeX, out sizeY, out sizeZ);
+        this.kernelThreadGroupSizeSnowFall = new Vector3(sizeX, sizeY, sizeZ);
+
+        this.gpuSnowFallComputeShader.SetBuffer(this.kernelIndexSnowFall, "_VertexBuffer", this.vertexBuffer);
+        this.gpuSnowFallComputeShader.SetBuffer(this.kernelIndexSnowFall, "_ScaleBuffer",  this.scaleBuffer);
+    }
+
+    /// <summary>
+    /// 更新時に呼び出されます。
+    /// </summary>
+    protected virtual void LateUpdate()
+    {
+        // 雪の頂点を動かします。
+        this.gpuSnowFallComputeShader.Dispatch(this.kernelIndexSnowFall,
+                                               (int)(this.particleCount / this.kernelThreadGroupSizeSnowFall.x),
+                                               1,
+                                               1);
+
+        this.gpuSnowFallComputeShader.SetVector("_MinRange",  this.minRange);
+        this.gpuSnowFallComputeShader.SetVector("_MaxRange",  this.maxRange);
+        this.gpuSnowFallComputeShader.SetVector("_MovePower", this.wind);
+
+        this.material.SetVector("_OriginPosition", base.transform.position);
+        this.material.SetVector("_CameraUp", Camera.main.transform.up);
+    }
+
+    /// <summary>
+    /// 描画時に呼び出されます。
+    /// </summary>
+    protected virtual void OnRenderObject()
+    {
+        this.material.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, this.particleCount);
+    }
+
+    /// <summary>
+    /// 破棄時に呼び出されます。
+    /// </summary>
+    protected virtual void OnDestroy()
+    {
+        // このインスタンスで生成したマテリアルを破棄します。
+        // マテリアルは破棄しない限りリソースを消費し続けます。
+        // ComputeBuffer も同様の理由で破棄します。
+
+        DestroyImmediate(this.material);
+        this.vertexBuffer.Release();
+        this.scaleBuffer.Release();
+    }
+
+    /// <summary>
+    /// 指定した数だけランダムに配置された頂点を生成します。
+    /// </summary>
+    /// <param name="count">
+    /// 生成する頂点の数。
+    /// </param>
+    /// <param name="minRange">
+    /// 生成する頂点の最小の範囲。
+    /// </param>
+    /// <param name="maxRange">
+    /// 生成する頂点の最大の範囲。
+    /// </param>
+    /// <returns>
+    /// ランダムに生成された頂点の配列。
+    /// </returns>
+    protected virtual Vector3[] GenerateRandomPositionVertexes
+        (int count, Vector3 minRange, Vector3 maxRange)
+    {
+        Vector3[] array = new Vector3[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = new Vector3(Random.Range(minRange.x, maxRange.x),
+                                   Random.Range(minRange.y, maxRange.y),
+                                   Random.Range(minRange.z, maxRange.z));
+        }
+
+        return array;
+    }
+
+    /// <summary>
+    /// 指定した数だけランダムな値を生成します。
+    /// </summary>
+    /// <param name="count">
+    /// 生成する値の数。
+    /// </param>
+    /// <param name="minRange">
+    /// 生成する値の最小の範囲。
+    /// </param>
+    /// <param name="maxRange">
+    /// 生成する値の最大の範囲。
+    /// </param>
+    /// <returns>
+    /// ランダムに生成された値の配列。
+    /// </returns>
+    protected virtual float[] GenerateRandomValues
+        (int count, float minRange, float maxRange)
+    {
+        float[] array = new float[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = Random.Range(minRange, maxRange);
+        }
+
+        return array;
+    }
+
+    #endregion Method
+}
---
+++ Assets/GPUSnowFall.shader
@@ -0,0 +1,142 @@
+﻿Shader "Custom/GPUSnowFall"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags
+        {
+            "Queue"           = "Transparent"
+            "IgnoreProjector" = "True"
+            "RenderType"      = "Transparent"
+        }
+
+        ZWrite Off
+        Cull   Off
+        Blend  SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma target   5.0
+            #pragma vertex   vertexShader
+            #pragma geometry geometryShader
+            #pragma fragment fragmentShader
+
+            #include "UnityCG.cginc"
+
+            StructuredBuffer<float3> _VertexBuffer;
+            StructuredBuffer<float>  _ScaleBuffer;
+
+            float3 _OriginPosition;
+            float3 _CameraUp;
+            
+            sampler2D _MainTex;
+            float4    _MainTex_ST;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv     : TEXCOORD0;
+            };
+
+            struct vertexOutput
+            {
+                float4 position : SV_POSITION;
+                float2 uv       : TEXCOORD0;
+                half4  color    : TEXCOORD1;
+                float  scale    : TESSFACTOR0;
+            };
+
+            float GetRandomNumber(float2 coord, int Seed)
+            {
+                return frac(sin(dot(coord.xy, float2(12.9898, 78.233)) + Seed) * 43758.5453);
+            }
+
+            // ------------------------------------------------------------------------------------
+            // VertexShader
+            // ------------------------------------------------------------------------------------
+
+            vertexOutput vertexShader(uint vertexID : SV_VertexID)
+            {
+                vertexOutput output;
+                
+                half value = vertexID % 10 / 10.0;
+
+                output.position = float4(_VertexBuffer[vertexID] + _OriginPosition, 1);
+                output.color    = half4(value, value, value, 1);
+                output.uv       = float2(0, 0);
+                output.scale    = _ScaleBuffer[vertexID];
+
+                return output;
+            }
+
+            // ------------------------------------------------------------------------------------
+            // GeometryShader
+            // ------------------------------------------------------------------------------------
+
+            [maxvertexcount(4)]
+            void geometryShader(point vertexOutput input[1],
+                inout TriangleStream<vertexOutput> outputStream)
+            {
+                vertexOutput output;
+                float4 position     = input[0].position;
+                float4 color        = input[0].color;
+                float  scale        = input[0].scale;
+                float randomValue   = GetRandomNumber(position.xy, 0);
+
+                for (int x = 0; x < 2; x++) 
+                {
+                    for (int y = 0; y < 2; y++) 
+                    {
+                        // ビルボードのメッシュを構成する 4 頂点になるようにする。
+
+                        output.position = position + float4(float2(x, y) * 0.5 * scale, 0, 0);
+
+                        // ビルボードの処理。
+                        // ObjSpaceViewDir は、オブジェクト空間から見たカメラの方向を算出する関数です。
+
+                        //float3 eyeVector   = normalize(ObjSpaceViewDir(output.position));
+                        //float3 rightVector = normalize(cross(eyeVector, _CameraUp));
+                        //output.position += float4((x - 0.5f) * rightVector, 0);
+                        //output.position += float4((y - 0.5f) * _CameraUp, 0);
+
+                        output.position = mul(UNITY_MATRIX_VP, output.position);
+                        output.uv       = TRANSFORM_TEX(float2(x, y), _MainTex);
+                        output.color    = color;
+                        output.scale    = scale;
+
+                        outputStream.Append(output);
+                    }
+                }
+
+                outputStream.RestartStrip();
+            }
+
+            // ------------------------------------------------------------------------------------
+            // FragmentShader
+            // ------------------------------------------------------------------------------------
+
+            fixed4 fragmentShader(vertexOutput input) : COLOR
+            {
+                //return input.color;
+
+                fixed4 color = tex2D(_MainTex, input.uv);
+
+                // 描画オブジェクトの前後関係が重要なときは、
+                // ZWrite を有効にして、α で削る。
+                //if (color.a < 0.3)
+                //{
+                //    discard;
+                //}
+
+                return color;
+            }
+
+            ENDCG
+        }
+    }
+}
